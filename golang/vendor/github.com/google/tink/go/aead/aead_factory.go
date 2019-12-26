630a75148bc49ecc5502020b235d4eefad668202
Elijah Soria <elijahsoria@google.com>
2019-12-26 10:19:59 -0800

    Add vendor files?

---
+++ golang/vendor/github.com/google/tink/go/aead/aead_factory.go
@@ -0,0 +1,104 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package aead
+
+import (
+	"fmt"
+
+	"github.com/google/tink/go/core/cryptofmt"
+	"github.com/google/tink/go/keyset"
+	"github.com/google/tink/go/core/primitiveset"
+	"github.com/google/tink/go/core/registry"
+	"github.com/google/tink/go/tink"
+)
+
+// New returns an AEAD primitive from the given keyset handle.
+func New(h *keyset.Handle) (tink.AEAD, error) {
+	return NewWithKeyManager(h, nil /*keyManager*/)
+}
+
+// NewWithKeyManager returns an AEAD primitive from the given keyset handle and custom key manager.
+func NewWithKeyManager(h *keyset.Handle, km registry.KeyManager) (tink.AEAD, error) {
+	ps, err := h.PrimitivesWithKeyManager(km)
+	if err != nil {
+		return nil, fmt.Errorf("aead_factory: cannot obtain primitive set: %s", err)
+	}
+	return newPrimitiveSet(ps), nil
+}
+
+// primitiveSet is an AEAD implementation that uses the underlying primitive set for encryption
+// and decryption.
+type primitiveSet struct {
+	ps *primitiveset.PrimitiveSet
+}
+
+// Asserts that primitiveSet implements the AEAD interface.
+var _ tink.AEAD = (*primitiveSet)(nil)
+
+func newPrimitiveSet(ps *primitiveset.PrimitiveSet) *primitiveSet {
+	ret := new(primitiveSet)
+	ret.ps = ps
+	return ret
+}
+
+// Encrypt encrypts the given plaintext with the given additional authenticated data.
+// It returns the concatenation of the primary's identifier and the ciphertext.
+func (a *primitiveSet) Encrypt(pt, ad []byte) ([]byte, error) {
+	primary := a.ps.Primary
+	var p = (primary.Primitive).(tink.AEAD)
+	ct, err := p.Encrypt(pt, ad)
+	if err != nil {
+		return nil, err
+	}
+	ret := make([]byte, 0, len(primary.Prefix) + len(ct))
+	ret = append(ret, primary.Prefix...)
+	ret = append(ret, ct...)
+	return ret, nil
+}
+
+// Decrypt decrypts the given ciphertext and authenticates it with the given
+// additional authenticated data. It returns the corresponding plaintext if the
+// ciphertext is authenticated.
+func (a *primitiveSet) Decrypt(ct, ad []byte) ([]byte, error) {
+	// try non-raw keys
+	prefixSize := cryptofmt.NonRawPrefixSize
+	if len(ct) > prefixSize {
+		prefix := ct[:prefixSize]
+		ctNoPrefix := ct[prefixSize:]
+		entries, err := a.ps.EntriesForPrefix(string(prefix))
+		if err == nil {
+			for i := 0; i < len(entries); i++ {
+				var p = (entries[i].Primitive).(tink.AEAD)
+				pt, err := p.Decrypt(ctNoPrefix, ad)
+				if err == nil {
+					return pt, nil
+				}
+			}
+		}
+	}
+	// try raw keys
+	entries, err := a.ps.RawEntries()
+	if err == nil {
+		for i := 0; i < len(entries); i++ {
+			var p = (entries[i].Primitive).(tink.AEAD)
+			pt, err := p.Decrypt(ct, ad)
+			if err == nil {
+				return pt, nil
+			}
+		}
+	}
+	// nothing worked
+	return nil, fmt.Errorf("aead_factory: decryption failed")
+}
---
+++ golang/vendor/github.com/google/tink/go/hybrid/hybrid.go
@@ -0,0 +1,77 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+// Package hybrid provides subtle implementations of the HKDF and EC primitives.
+//
+// The functionality of Hybrid Encryption is represented as a pair of
+// primitives (interfaces):
+//
+//  * HybridEncrypt for encryption of data
+//
+//  * HybridDecrypt for decryption of data
+//
+// Implementations of these interfaces are secure against adaptive chosen
+// ciphertext attacks. In addition to plaintext the encryption takes an extra
+// parameter contextInfo, which usually is public data implicit from the
+// context, but should be bound to the resulting ciphertext, i.e. the
+// ciphertext allows for checking the integrity of contextInfo (but there are
+// no guarantees wrt. the secrecy or authenticity of contextInfo).
+//
+// Example:
+//
+//   package main
+//
+//   import (
+//       "github.com/google/tink/go/hybrid"
+//       "github.com/google/tink/go/core/registry"
+//       "github.com/google/tink/go/keyset"
+//   )
+//
+//   func main() {
+//
+//       kh , err := keyset.NewHandle(hybrid.ECIESHKDFAES128CTRHMACSHA256KeyTemplate())
+//       if err != nil {
+//           //handle error
+//       }
+//       h := hybrid.NewHybridEncrypt(kh)
+//
+//       ct, err = h.Encrypt([]byte("secret message"), []byte("context info"))
+//       if err != nil {
+//           // handle error
+//       }
+//
+//       khd , err := keyset.NewHandle( .....); /// get a handle on the decryption key material
+//       hd := hybrid.NewHybridDecrypt(khd)
+//
+//       pt, err := hd.Decrypt(ct, []byte("context info"))
+//       if err != nil {
+//           // handle error
+//       }
+//   }
+package hybrid
+
+import (
+	"fmt"
+
+	"github.com/google/tink/go/core/registry"
+)
+
+func init() {
+	if err := registry.RegisterKeyManager(newECIESAEADHKDFPrivateKeyKeyManager()); err != nil {
+		panic(fmt.Sprintf("hybrid.init() failed: %v", err))
+	}
+	if err := registry.RegisterKeyManager(newECIESAEADHKDFPublicKeyKeyManager()); err != nil {
+		panic(fmt.Sprintf("hybrid.init() failed: %v", err))
+	}
+}
